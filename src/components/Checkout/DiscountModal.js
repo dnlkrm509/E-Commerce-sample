1b8d4302f3e9680b2e6d3825e22a77061cfb2619
Danial <danial@email.com>
2022-10-09 21:54:40 +0100

    compelete discount modal and store the discount value in localStorage

--- src/components/Checkout/DiscountModal.js
+++ src/components/Checkout/DiscountModal.js
@@ -6,28 +6,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { uiActions } from "../../store/ui-slice";
 
 const DiscountModal = (props) => {
-    const [shippingCompany, setShippingCompany] = useState(localStorage.getItem('company'));
-    const [shippingService, setShippingService] = useState({
-        type: localStorage.getItem('type'),
-        duration: localStorage.getItem('duration'),
-        price: localStorage.getItem('price')
-    });
+    const [storedDiscount, setStoredDiscount] = useState(localStorage.getItem('discount'));
     const dispatch = useDispatch();
-    const [shipping, setShipping] = useState('Company');
     const isDiscountModal = useSelector(state => state.ui.isDiscountModal);
-
-    const shippingCompanyHandler = () => {
-        setShipping('Company');
-    };
-
-    const shippingServiceHandler = () => {
-        setShipping('Service');
-    };
     
     const hideModalHandler = () => {
         dispatch(uiActions.hideWishlistModal());
-        console.log('Company: ', shippingCompany);
-        console.table('Service: ', shippingService);
+        console.log('Discount: ', storedDiscount);
     };
 
     return (
@@ -36,114 +21,44 @@ const DiscountModal = (props) => {
                 <section className="w-full flex flex-col items-center justify-center p-[40px_24px_24px] gap-[40px] absolute left-0 bottom-0 bg-[#fff] rounded-[12px_12px_0_0]">
                 <div className="flex justify-between items-center p-0 order-0 self-stretch grow-0 mb-[40px]">
                     <h2 className="font-[Manrope] not-italic font-bold text-[20px] leading-[27px] text-[#414040] order-0 grow-0">
-                        Choose Shipping {shipping}
+                        Discount
                     </h2>
                     <button className="text-[24px]" onClick={hideModalHandler}>
                         <FontAwesomeIcon icon={faClose} />
                     </button>
                 </div>
-                    {shipping === 'Company' && (
-                    <div className="flex flex-col items-start p-0 gap-[12px] order-1 self-stretch grow-0">
-                        <button 
-                            onClick={() => { setShippingCompany('JN Express'); localStorage.setItem('company', 'JN Express') }}
-                            className="flex w-full items-start p-[10px_12px] gap-[10px] bg-[#fff] rounded-[4px] order-0 self-stretch grow-0">
-                            <p className="font-[Manrope] not-italic font-medium text-[14px] leading-[19px] text-[#414040] order-0 grow-1">
-                                JN Express
+                    <div className="flex flex-col items-start p-0 gap-[32px] bg-[#fff] order-1 self-stretch grow-0">
+                        <div className="flex flex-col w-full items-start p-0 gap-[8px] order-0 self-stretch grow-0">
+                            <p className="font-[Manrope] not-italic font-bold text-[20px] leading-[27px] text-[#414040] order-0 grow-0">
+                                80% Cashback
                             </p>
-                        </button>
-                        <div className="flex items-start w-full p-0 gap-[10px] h-[1px] order-1 bg-[#cfcfcf] grow-0"></div>
-                        <button 
-                            onClick={() => { setShippingCompany('Fastest Express'); localStorage.setItem('company', 'Fastest Express') }}
-                            className="flex items-start p-[10px_12px] gap-[10px] bg-[#fff] rounded-[4px] order-2 self-stretch grow-0">
-                            <p className="font-[Manrope] not-italic font-medium text-[14px] leading-[19px] text-[#414040] order-0 grow-1">
-                                Fastest Express
+                            <p className="font-[DMSans] not-italic font-normal text-[12px] leading-[18px] text-[#414040] order-1 grow-0">
+                                Available until <span className="text-[#4494B5]">30 Jun 2021</span>
                             </p>
-                        </button>
-                        <div className="flex items-start w-full p-0 gap-[10px] h-[1px] order-3 bg-[#cfcfcf] grow-0"></div>
-                        <button 
-                            onClick={() => { setShippingCompany('Flying Express'); localStorage.setItem('company', 'Flying Express') }} 
-                            className="flex items-start p-[10px_12px] gap-[10px] bg-[#fff] rounded-[4px] order-4 self-stretch grow-0">
-                            <p className="font-[Manrope] not-italic font-medium text-[14px] leading-[19px] text-[#414040] order-0 grow-1">
-                                Flying Express
-                            </p>
-                        </button>
-                    </div>
-                    )}
-                    {shipping === 'Service' && (
-                        <div className="flex flex-col items-start p-0 gap-[12px] order-1 self-stretch grow-0">
-                            <button 
-                                onClick={() => {
-                                    setShippingService({type: 'Same Day', duration: '1 Day', price: 56000});
-                                    localStorage.setItem('type', 'Same Day');
-                                    localStorage.setItem('duration', '1 Day');
-                                    localStorage.setItem('price', 56000) }}
-                                className="flex items-center p-[10px_12px] gap-[10px] bg-[#fff] rounded-[4px] order-0 self-stretch grow-0">
-                                <div className="flex flex-col items-start p-0 gap-[4px] order-0 self-stretch grow-1">
-                                    <p className="font-[Manrope] not-italic font-medium text-[14px] leading-[19px] text-[#414040] order-0 grow-0">
-                                        Same Day
-                                    </p>
-                                    <p className="font-[DMSans] not-italic font-normal text-[12px] leading-[18px] text-[#696969] order-1 self-stretch grow-0">
-                                        EST. 1 Day
-                                    </p>
-                                </div>
-                                <p className="font-[Manrope] not-italic font-bold text-[14px] leading-[19px] text-right w-[170.5px] text-[#414040] order-1 grow-0">
-                                    RP  56000
-                                </p>
-                            </button>
-                            <div className="flex items-start self-stretch p-0 gap-[10px] h-[1px] order-1 bg-[#cfcfcf] grow-0"></div>
-                            <button 
-                                onClick={() => {
-                                    setShippingService({type: 'Express', duration: '2-3 Day', price: 40000});
-                                    localStorage.setItem('type', 'Express');
-                                    localStorage.setItem('duration', '2-3 Day');
-                                    localStorage.setItem('price', 40000) }}
-                                className="flex items-center p-[10px_12px] gap-[10px] bg-[#fff] rounded-[4px] order-2 self-stretch grow-0">
-                                <div className="flex flex-col items-start p-0 gap-[4px] order-0 self-stretch grow-1">
-                                    <p className="font-[Manrope] not-italic font-medium text-[14px] leading-[19px] text-[#414040] order-0 grow-0">
-                                        Express
-                                    </p>
-                                    <p className="font-[DMSans] not-italic font-normal text-[12px] leading-[18px] text-[#696969] order-1 self-stretch grow-0">
-                                        EST. 2-3 Day
-                                    </p>
-                                </div>
-                                <p className="font-[Manrope] not-italic font-bold text-[14px] leading-[19px] text-right w-[170.5px] text-[#414040] order-1 grow-0">
-                                    RP  40000
-                                </p>
-                            </button>
-                            <div className="flex items-start self-stretch p-0 gap-[10px] h-[1px] order-3 bg-[#cfcfcf] grow-0"></div>
-                            <button 
-                                onClick={() => {
-                                    setShippingService({type: 'Regular', duration: '5-14 Day', price: 27000});
-                                    localStorage.setItem('type', 'Regular');
-                                    localStorage.setItem('duration', '5-14 Day');
-                                    localStorage.setItem('price', 27000) }}
-                                className="flex items-center p-[10px_12px] gap-[10px] bg-[#fff] rounded-[4px] order-4 self-stretch grow-0">
-                                <div className="flex flex-col items-start p-0 gap-[4px] order-0 grow-1">
-                                    <p className="font-[Manrope] not-italic font-medium text-[14px] leading-[19px] text-[#414040] order-0 grow-0">
-                                        Regular
-                                    </p>
-                                    <p className="font-[DMSans] not-italic font-normal text-[12px] leading-[18px] text-[#696969] order-1 self-stretch grow-0">
-                                        EST. 5-14 Day
-                                    </p>
-                                </div>
-                                <p className="font-[Manrope] not-italic font-bold text-[14px] leading-[19px] text-right w-[170.5px] text-[#414040] order-1 grow-0">
-                                    RP 27000
-                                </p>
-                            </button>
                         </div>
-                    )}
-                    <div className="mt-[40px] flex items-center p-0 gap-[16px] self-stretch grow-0 order-4">
-                        <button  
-                            onClick={shippingServiceHandler}
-                            className="bg-[#9D44B5] w-[50%] box-border font-bold font-[Manrope] text-[14px] text-center not-italic p-[11px_29px] text-[#fff] rounded-[4px] grow-1 order-1"
-                        >
-                            Shipping Service
-                        </button>
+                        <div className="flex flex-col items-start p-0 gap-[12px] order-1 grow-0 self-stretch">
+                            <p className="font-[Manrope] not-italic font-bold text-[16px] leading-[22px] text-[#414040] self-stretch order-0 grow-0">
+                                Terms & Condition
+                            </p>
+                            <div className="flex flex-col items-start p-[8px] gap-[10px] rounded-[4px] bg-[#f8f8f8] order-1 self-stretch grow-0 w-full">
+                                <ul className="list list-disc list-inside font-[DMSans] not-italic font-normal text-[12px] leading-[18px] text-[#414040] order-0 grow-1 w-[100% - 20px]">
+                                    <li>This voucher is applicable to all kinds of products.</li>
+                                    <li>This voucher can only be used for transactions using E-Balance method.</li>
+                                    <li>This voucher can only be used once.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="mt-[40px] flex items-center self-stretch grow-0 order-2">
                         <button
-                            onClick={shippingCompanyHandler} 
-                            className="bg-[#fff] w-[50%] border border-[#9D44B5] font-bold font-[Manrope] box-border text-[14px] text-center not-italic p-[11px_20px] text-[#9D44B5] rounded-[4px] grow-1 order-0"
+                            onClick={() => { 
+                                setStoredDiscount('80% Cashback');
+                                localStorage.setItem('discount', '80% Cashback');
+                                hideModalHandler();
+                            }}  
+                            className="bg-[#9D44B5] w-full box-border font-bold font-[Manrope] text-[14px] text-center not-italic p-[11px_29px] text-[#fff] rounded-[4px]"
                         >
-                            Shipping Company
+                            Confirm
                         </button>
                     </div>
                 
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import BottomNav from '../UI/Nav/BottomNav';
 import TopNav from '../UI/Nav/TopNav';
 import ProductsCategories from './ProductsCategories';
@@ -7,7 +7,15 @@ import Ad2 from '../../assets/img/Ad/Ad2.png';
 import PopularProductsList from './PopularProducts';
 import NewArivalsList from './NewArivalsList';
 
+let isFirstTime = true;
+
 const Home = () => {
+    useEffect(() => {
+        if (isFirstTime) {
+            localStorage.clear();
+            isFirstTime = false;
+        }
+    }, [isFirstTime])
     return (
         <div className='felx flex-col justify-between w-full px-[1.5rem]'>
             <TopNav title='Home' notification={true} />
